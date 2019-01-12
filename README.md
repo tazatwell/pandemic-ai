0e0f12381db43dae11812eac4f8ac666b0bcf6e1
Taz Atwell <tazatwell@gmail.com>
2019-01-12 11:58:00 -0800

    creating game board

---
+++ README.md
@@ -0,0 +1,5 @@
+README.md:
+
+This repo is an ai that solves the board game Pandemic.
+
+Special thanks to Matt Leacock and Z-Man Games, the creator and publisher of Pandemic.
---
+++ pandemic.py
@@ -0,0 +1,35 @@
+#pandemic.py: The board game pandemic
+
+#edge class:
+class Edge:
+	start = "San Francisco";
+	end = "Tokyo";
+
+
+#city class:
+class City:
+	name = "San Francisco";
+	color = "Blue";
+
+	#list of edges from city to neighboring cities
+	neighbors = [];
+
+	#class constructor
+	def _init_(self, new_name, new_color, new_neighbors):
+		self.name = new_name;
+		self.color = new_color;
+		self.neighbors = new_neighbors;
+	
+#get list of cities
+citiesList = [];
+
+f = open('cities.txt', r);
+for i in f:
+	citiesList.append(i);
+
+for i in citiesList:
+	print(i);
+
+
+
+
